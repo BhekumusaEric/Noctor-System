0528d8fd9b93299cf82e54740b5afb8162fc06b8
bhntshwcjc025 <bhntshwcjc025@student.wethinkcode.co.za>
2025-12-06 22:02:13 +0200

    structure set up

---
+++ src/main/java/com/wethinkcode/demo/NoctorSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wethinkcode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoctorSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoctorSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wethinkcode/demo/NoctorSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wethinkcode.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoctorSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
