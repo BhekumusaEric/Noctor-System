3f7f037c09e7d264f91bba23bb00971d37bff96b
bhntshwcjc025 <bhntshwcjc025@student.wethinkcode.co.za>
2025-12-07 09:08:13 +0200

    mobile version added

---
+++ mobile-app/src/main/java/com/wethinkcode/noctor/MainActivity.java
@@ -0,0 +1,45 @@
+package com.wethinkcode.noctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        // To find your local network IP on Linux/macOS, run `ifconfig` or `ip addr` in your terminal.
+        // On Windows, run `ipconfig`.
+        // Replace 192.168.1.100 with your actual IP address.
+        webView.loadUrl("http://192.168.8.114:8081");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
